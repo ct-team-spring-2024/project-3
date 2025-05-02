4b68e80c5602b7c2f812879b08ab81f5c6552bae
Mohammad Broughani <gitmbroughani81@gmail.com>
2025-05-02 20:42:15 +0330

    ~

---
+++ client/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	logrus.Info("Starting: Client")
+}
